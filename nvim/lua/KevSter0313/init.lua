b162815269a54061df34b34c3b7e9724fb2eb42a
KevSter0313 <sosak0313@gmail.com>
2022-12-26 23:56:33 +0100

    new nvim config

--- nvim/lua/KevSter0313/init.lua
+++
@@ -1,2 +0,0 @@
-require("KevSter0313.set")
-require("KevSter0313.remap")
--- zsh/custom/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias v="nvim"
-alias cls="clear"
-alias icat="kitty +kitten icat"
-start () {
-    local dir=$1
-    nautilus $dir &
-}
